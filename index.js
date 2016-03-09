d642a5794c772bfc0f0a28ce0195b9e8f9846ad4
R.A. Lucas <richard@richardalucas.com>
2016-03-08 21:16:49 -0700

    updated to function

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-var services = require('factory-create').createFromIndex();
-module.exports = services;
+var fs = require('fs');
+
+var pkg = fs.readFileSync('package.json', 'utf-8');
+var dependencies = Object.keys(
+  JSON.parse(pkg).dependencies
+);
+
+function createInstance(klass, params) {
+  var Klass = require(klass);
+  return new Klass(params);
+}
+
+module.exports = function(configs) {
+  return dependencies.map(function(dependency) {
+    var service = {};
+    var name = dependency.split(/\-|\.|\_/)[0];
+    var config = configs[name];
+    var instance = createInstance(dependency, config);
+    service[name] = instance;
+    return service;
+  }).reduce(function(pv, cv) {
+    return Object.assign(pv, cv);
+  }, {});
+};
